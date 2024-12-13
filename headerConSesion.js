7672a2f7970664630160219cc06184e5701c89e3
fgarmol <fgarmol2808@g.educaand.es>
2024-12-13 18:04:18 +0100

    header cambia segun la clase

--- headerConSesion.js
+++ headerConSesion.js
@@ -1,17 +1,23 @@
-window.addEventListener("load", function(){
-    let header = document.getElementById('app-header');
-    
-    let logo = document.createElement('div');
-    logo.className = 'logo';
-    let img = document.createElement('img');
-    img.src = '/img/download.png';
-    logo.appendChild(img);
-  
-    
-    let strong = document.createElement('strong');
-    strong.textContent = '*Inserta texto de la cabecera prro*';
-  
-    
-    header.appendChild(logo);
-    header.appendChild(strong);
-})
+window.addEventListener("load", function () {
+  let header = document.getElementById("app-header");
+
+  let logo = document.createElement("div");
+  logo.className = "logo";
+  let img = document.createElement("img");
+  img.src = "/img/download.png";
+  logo.appendChild(img);
+
+  let strong = document.createElement("strong");
+  if ((this.document.getElementById("app-header").className == "alumno")) {
+    strong.textContent = "Área Alumno";
+  }
+  if ((this.document.getElementById("app-header").className == "profesor")) {
+    strong.textContent = "Área Profesor";
+  }
+  if ((this.document.getElementById("app-header").className == "admin")) {
+    strong.textContent = "Área Admin";
+  }
+
+  header.appendChild(logo);
+  header.appendChild(strong);
+});
